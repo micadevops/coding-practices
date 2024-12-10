a45a849bff27b205da34b17990ca28ec12f41b87
micaela-marrero <micaela.marrero@globant.com>
2024-12-10 20:02:47 +0100

    dia 1 y 2

--- calendario/dia1/main.js
+++ calendario/dia1/main.js
@@ -1,5 +1,6 @@
 function prepareGifts(gifts) {
     
+
     const giftOutDuplicate = gifts.filter((item, index) => gifts.indexOf(item) === index);
     giftOutDuplicate.sort((a, b) => a - b);
     console.log(giftOutDuplicate)
@@ -9,3 +10,44 @@ function prepareGifts(gifts) {
 const gifts = [1, 40, 30, 31, 30, 20, 21]
 
 prepareGifts(gifts)
+
+
+/** 
+ * 
+
+Para gifts = [1, 40, 30, 31, 30, 20, 21]:
+
+Primer elemento el filter dice:
+
+item = 1, index = 0
+
+gifts.indexOf(1) = 0
+
+¿0 === 0? → Sí, se incluye 1.
+
+Segundo elemento el filter dice:
+
+item = 40, index = 1
+
+gifts.indexOf(40) = 1 aparece por primera vez en el indice 1
+
+¿1 === 1? → Sí, se incluye 40.
+
+Tercer elemento en el filter dice:
+
+item = 30, index = 2
+
+gifts.indexOf(30) = 2 la primera vez que aparece el 30, es en el indice 2.
+
+¿2 === 2? → Sí, se incluye 30.
+Cuarto elemento:
+
+item = 31, index = 3
+gifts.indexOf(31) = 3
+¿3 === 3? → Sí, se incluye 31.
+Quinto elemento:
+
+item = 30, index = 4
+gifts.indexOf(30) = 2 (la primera vez que apareció 30 fue en el índice 2).
+¿2 === 4? → No, se descarta 30.
+ */
--- calendario/dia2/main.js
+++ calendario/dia2/main.js
@@ -1,4 +1,4 @@
-const nombres = ["Micaela"]
+const names = ["Micaela", "Juan", "Pedrpppppppppp"]
 
 // console.log("************")
 // nombres.forEach((item) => {
@@ -16,15 +16,55 @@ const nombres = ["Micaela"]
 //     }, '');
 
 
-const maxLength = nombres.reduce((acc, elemento) => {
-    return acc.length > elemento.length ? acc : elemento;
-}, '').length;
 
-const border = '*'.repeat(maxLength + 4 );
-console.log(border);
+/** El reduce se usa para recorrer un array y hacer algo con cada uno de sus elementos.
+En este caso, vamos a empezar el acc con un elemento ' ' , y vamos a comparar el largo del 
+acc con el largo del elemento, si el acc es mayor se queda con el acc, 
+si no lo es se queda con el elemento.
 
-nombres.forEach((item) => {
-    console.log(`* ${item} ${' '.repeat(maxLength - item.length)}*`)
+Luego, a ese elemento, le vamos a hacer un .length, y guardamos ese largo en maxLength
+**/
 
-})
-console.log(border);
+// const maxLength = nombres.reduce((acc, elemento) => {
+//     return acc.length > elemento.length ? acc : elemento;
+// }, '').length;
+
+// const border = '*'.repeat(maxLength + 3); // el +4 es para que agregue unos especios para que se vea bien
+// console.log(border);
+
+// nombres.forEach((item) => {
+//     console.log(`*${item}${' '.repeat(maxLength - item.length)}*`) //voy a agregar especios vacios extras, el mayor largo, menos el largo del elemento
+
+// })
+
+// names.forEach((item) => {
+//     result += (`* ${item} ${' '.repeat(maxLength - item.length)}\n *`)
+// })
+
+// console.log(border);
+
+
+
+function createFrame(names) {
+
+    const maxLength = names.reduce((acc, elemento) => {
+        return acc.length > elemento.length ? acc : elemento;
+    }, '').length;
+  
+
+  
+    const border = '*'.repeat(maxLength + 4 );
+    let result = border + '\n';
+  
+  
+    names.forEach((item) => {
+        result += (`* ${item}${' '.repeat(maxLength - item.length)} *\n`)
+    })
+  
+    result+=border
+  
+    return result
+  }
+
+  const resulta = createFrame(names)
+  console.log(resulta)
