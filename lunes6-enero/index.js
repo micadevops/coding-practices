992d645fc606405e19decf0825c5a43cc90c51ed
micaela-marrero <micaela.marrero@globant.com>
2025-01-09 21:25:39 +0100

    more excercies

--- lunes6-enero/index.js
+++ lunes6-enero/index.js
@@ -1,66 +1,137 @@
-//EJERCICIO 1
-function sumar (numero1, numero2){
-    return numero1 + numero2
-}
+// //EJERCICIO 1
+// function sumar (numero1, numero2){
+//     return numero1 + numero2
+// }
 
-const resultado = sumar(10, 20)
+// const resultado = sumar(10, 20)
 
-console.log(resultado)
-
-
-//EJERCICIO 2
-
-function esPar (numero) {
-
-    const resultado = numero % 2
-    if (resultado === 0) {
-        return true
-    }else{
-        return false
-    }
-}
+// console.log(resultado)
 
 
-const resultadoEsPar = esPar (20)
+// //EJERCICIO 2
 
-if (resultadoEsPar) {
+// function esPar (numero) {
 
-    console.log ( "El numero es par")
-}else {
-    console.log ("El numero es impar")
-}
+//     const resultado = numero % 2
+//     if (resultado === 0) {
+//         return true
+//     }else{
+//         return false
+//     }
+// }
 
 
-//EJERCICIO 3
+// const resultadoEsPar = esPar (20)
+
+// if (resultadoEsPar) {
+
+//     console.log ( "El numero es par")
+// }else {
+//     console.log ("El numero es impar")
+// }
 
 
-function mostrarNumeros () {
+// //EJERCICIO 3
+
+
+// function mostrarNumeros () {
     
-    for ( let i = 10;  i >= 0; i--){
+//     for ( let i = 10;  i >= 0; i--){
 
-        console.log (i)
+//         console.log (i)
 
-    }
-}
-
-
-mostrarNumeros()
+//     }
+// }
 
 
-function cadenaReves (texto) {
+// mostrarNumeros()
 
 
-    let nuevaPalabra = "";
+// function cadenaReves (texto) {
 
 
-    for (let i = texto.length -1 ; i >=0; i -- ){
-        nuevaPalabra += texto [i]
+//     let nuevaPalabra = "";
+
+
+//     for (let i = texto.length -1 ; i >=0; i -- ){
+//         nuevaPalabra += texto [i]
        
+//     }
+
+//     return nuevaPalabra
+// }
+
+
+// const nuevaPalabraResultado = cadenaReves("Micaela")
+// console.log(nuevaPalabraResultado)
+
+
+
+
+// numeros[0] == 1
+//numeroNuevo == 0 
+
+// numeroMasGrande == 1
+
+// // numeros[1] == 10
+// numeroMasGrane == 1
+
+
+// numeroMasGrande == 10
+
+
+// // numeros[2] == 2
+// numeroMasGrande == 10
+
+// let numeroMasGrande
+
+
+
+// function numeroMasGrande(){
+
+//     const numeros = [1, 10, 2, 4, 55, 6, 7, 8, 9, 10, 20, 1, 30, 20, 4];
+//     let numeroMasGrande = 0
+
+//     for (let i=0; i < numeros.length ; i ++) {
+
+//         if (numeros[i] > numeroMasGrande){
+//             numeroMasGrande = numeros[i]
+//         }
+
+//     }
+
+//     return numeroMasGrande;
+// }
+
+
+// const resultado = numeroMasGrande()
+// console.log(resultado)
+
+
+// let numeroMaximo = 10;
+
+
+// while(numeroMaximo >= 0) {
+//     console.log(numeroMaximo)
+//     numeroMaximo = numeroMaximo - 1  
+// }
+
+
+
+//NO terminado //TODO: terminar ejercicio 
+const palabra  = "amar"
+const arrayPalabra = palabra.split('');
+
+
+for (let i=0; i < arrayPalabra.length; i ++) {
+
+    console.log("Primera letra")
+    console.log(arrayPalabra[i])
+
+    for (let i = arrayPalabra.length ; i > 0 ; i --) {
+
+        console.log("Segunda letra")
+        console.log(arrayPalabra[i -1])
+    
     }
-
-    return nuevaPalabra
 }
-
-
-const nuevaPalabraResultado = cadenaReves("Micaela")
-console.log(nuevaPalabraResultado)
